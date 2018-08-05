4dca3dea731d9266481b4358174cb000b6038f5e
Jakub Stacho <stacho.jakub@gmail.com>
2018-08-05 16:24:32 +0200

    [Player] Added Player project

---
+++ Client/GhostGameClient/GhostGamePlayer/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GhostGamePlayer
+{
+  public partial class Game : Form
+  {
+    public Game()
+    {
+      InitializeComponent();
+    }
+
+    private void Game_Load(object sender, EventArgs e)
+    {
+      this.TopMost = true;
+      this.FormBorderStyle = FormBorderStyle.None;
+      this.WindowState = FormWindowState.Maximized;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
